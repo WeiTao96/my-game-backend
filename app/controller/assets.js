038955e401c1441f3da5787a11e1b8acf59970ad
lwt <359584651@qq.com>
2021-01-10 23:32:31 +0800

    feat: 土地接口功能实现

--- app/controller/assets.js
+++ app/controller/assets.js
@@ -11,6 +11,17 @@ class AssetsController extends Controller {
         const res = await this.ctx.service.assets.getAllLand()
         this.ctx.body = res
     }
+    async handleRent() {
+        const id = this.ctx.params.id;
+        const update = {
+            name: '农田',
+            income: 100,
+            s_type: 1,   // 1 为农田
+            type: 1
+        }
+        const res = await this.ctx.service.assets.handleLandUpdate(id, update)
+        this.ctx.body = res
+    }
 }
 
 module.exports = AssetsController;
--- app/service/assets.js
+++ app/service/assets.js
@@ -3,14 +3,18 @@ const Service = require('egg').Service;
 class AssetsService extends Service {
     async addNewLand() {
         const land = {
-            name: '农田',
-            income: 100,
-            outcome: 20,
+            name: '荒地',
+            income: 0,
+            outcome: 0,
             type: 0,
             status: true,
             level: 0
         }
         const res = await this.ctx.model.Land.create(land)
+        return res
+    }
+    async handleLandUpdate(id, update) {
+        const res = await this.ctx.model.Land.findByIdAndUpdate(id, update)
         return res
     }
     async getAllLand() {
